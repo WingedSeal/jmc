3c0ecfe90b163c3cb015c0615be5e4f891ca95ad
WingedSeal <firm09719@gmail.com>
2021-12-27 10:13:55 +0700

    Improve logging
    - Add 'logs/' to .gitignore
    - Add is_log_file: bool to Logger parameter
    - Logging file default path will now be automatically generated depending
    on date

--- jmc/log.py
+++ jmc/log.py
@@ -1,14 +1,25 @@
 import logging
+from datetime import datetime
+from pathlib import Path
 
 FORMATTER = logging.Formatter(
     '%(asctime)s | %(name)s | %(levelname)s | %(message)s')
 
 
-def Logger(name: str, level: int, file_path: str = "test.log", is_stream: bool = True) -> logging.Logger:
+def Logger(name: str, level: int, file_path: str = None, is_stream: bool = True, is_log_file: bool = True) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    if file_path:
-        file_handler = logging.FileHandler(file_path)
+
+    if is_log_file:
+        if file_path is None:
+            now = datetime.now()
+            file_path = Path(
+                f'logs/{now.strftime("%Y-%b")}/{now.day}{now.strftime("-%m-%Y")}.log')
+        else:
+            file_path = Path(file_path)
+        file_path: Path
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        file_handler = logging.FileHandler(file_path.resolve())
         file_handler.setFormatter(FORMATTER)
         logger.addHandler(file_handler)
     if is_stream:
