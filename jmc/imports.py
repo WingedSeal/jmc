3b2b9125bd932325d5d9a72a033750cb31624923
WingedSeal <firm09719@gmail.com>
2021-12-27 10:26:05 +0700

    Automatically add .jmc to import string

--- jmc/imports.py
+++ jmc/imports.py
@@ -12,6 +12,9 @@ def handle_imports(string: str, pack_global: PackGlobal) -> str:
     imports_match: List[re.Match] = re.finditer(
         r'@import [\'\"](.*?)[\'\"]; ', string)
     for import_match in imports_match:
+        path: str = import_match.groups()[0]
+        if not path.endswith('.jmc'):
+            path += '.jmc'
         pack_global.imports.append(
-            Module(import_match.groups()[0], pack_global))
+            Module(path, pack_global))
     return re.sub(r'@import [\'\"](.*?)[\'\"]; ', '', string)
