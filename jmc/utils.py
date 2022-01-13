2ab9bd031af7d074f4ecd1bdce12903b0a89a8d6
WingedSeal <firm09719@gmail.com>
2022-01-13 20:31:45 +0700

    Move match_bracket to BracketRegex

--- jmc/utils.py
+++ jmc/utils.py
@@ -167,35 +167,22 @@ class BracketRegex:
             del strings[index-1]
         return tuple(strings)
 
-    def add_remove_list(self, start_group: int):
-        self.remove_list += [start_group+1, start_group+2, start_group+3]
+    def match_bracket(self, bracket: str, start_group: int) -> str:
+        """Generate regex for matching bracket, need to be used with BracketRegex
+        Example:
+        ```
+        '(group1)' + match_bracket('{}', 2, bracket_regex) + '(group3)' + match_bracket('()', 4, bracket_regex)
+        ```
 
+        Args:
+            bracket (str): A string with length of 2, containing openning and closing of that bracket type. For example `{}`
+            start_group (int): A group(start at 1) which match_bracket should be. (Assuming 1 match_bracket use up 1 group)
+            bracket_regex (BracketRegex): Store stuff for compiling groups()
 
-def match_bracket(bracket: str, start_group: int, bracket_regex: BracketRegex) -> str:
-    """Generate regex for matching bracket, need to be used with BracketRegex
-    Example:
-    ```
-    '(group1)' + match_bracket('{}', 2, bracket_regex) + '(group3)' + match_bracket('()', 4, bracket_regex)
-    ```
-
-    Args:
-        bracket (str): A string with length of 2, containing openning and closing of that bracket type. For example `{}`
-        start_group (int): A group(start at 1) which match_bracket should be. (Assuming 1 match_bracket use up 1 group)
-        bracket_regex (BracketRegex): Store stuff for compiling groups()
-
-    Returns:
-        str: Regex pattern for matching brackets
-    """
-    start_group += bracket_regex.match_bracket_count * 3
-    bracket_regex.match_bracket_count += 1
-    bracket_regex.add_remove_list(start_group)
-
-    # pattern = f'(\\{bracket[0]}((?:(?:(["\'])(?:(?=(\\\\?))\\'
-    # pattern += str(start_group + 3)
-    # pattern += '.)*?\\'
-    # pattern += str(start_group + 2)
-    # pattern += f'|[^{bracket[1]}{bracket[0]}])+|(?'
-    # pattern += str(start_group)
-    # pattern += f'))*+)\\{bracket[1]})'
-
-    return f'(\\{bracket[0]}((?:(?:(["\'])(?:(?=(\\\\?))\\{start_group+3}.)*?\\{start_group+2}|[^{bracket[1]}{bracket[0]}])+|(?{start_group}))*+)\\{bracket[1]})'
+        Returns:
+            str: Regex pattern for matching brackets
+        """
+        start_group += self.match_bracket_count * 3
+        self.match_bracket_count += 1
+        self.remove_list += [start_group, start_group+2, start_group+3]
+        return f'(\\{bracket[0]}((?:(?:(["\'])(?:(?=(\\\\?))\\{start_group+3}.)*?\\{start_group+2}|[^{bracket[1]}{bracket[0]}])+|(?{start_group}))*+)\\{bracket[1]})'
