227f40534f35988cc43fad6ce92b4dc19aae55fc
WingedSeal <firm09719@gmail.com>
2022-03-23 18:45:24 +0700

    Add MinecraftSyntaxWarning

--- jmc/exception.py
+++ jmc/exception.py
@@ -4,25 +4,35 @@ from .log import Logger
 logger = Logger(__name__)
 
 
+def log(self: object, args: tuple):
+    logger.warning(f"{self.__class__.__name__}\n{args[0]}")
+
+
 class JMCSyntaxException(SyntaxError):
     def __init__(self, *args: object) -> None:
-        logger.warning(f"JMCSyntaxException\n{args[0]}")
+        log(self, args)
         super().__init__(*args)
 
 
 class JMCSyntaxWarning(SyntaxWarning):
     def __init__(self, *args: object) -> None:
-        logger.warning(f"JMCSyntaxWarning\n{args[0]}")
+        log(self, args)
         super().__init__(*args)
 
 
 class JMCFileNotFoundError(FileNotFoundError):
     def __init__(self, *args: object) -> None:
-        logger.warning(f"JMCFileNotFoundError\n{args[0]}")
+        log(self, args)
         super().__init__(*args)
 
 
 class JMCDecodeJSONError(ValueError):
     def __init__(self, *args: object) -> None:
-        logger.warning(f"JMCDecodeJSONError\n{args[0]}")
+        log(self, args)
+        super().__init__(*args)
+
+
+class MinecraftSyntaxWarning(SyntaxError):
+    def __init__(self, *args: object) -> None:
+        log(self, args)
         super().__init__(*args)
