2c8706d93d4e8277b6ae4fc7287af67ca7f79e3e
WingedSeal <firm09719@gmail.com>
2022-03-28 16:32:58 +0700

    Turn AST into a minecraft command(s)

--- jmc/command/condition.py
+++ jmc/command/condition.py
@@ -1,12 +1,39 @@
-from textwrap import indent
-from typing import Union
+from dataclasses import dataclass
+from typing import Optional, Union
+
 from ..tokenizer import TokenType, Tokenizer, Token
 from ..exception import JMCSyntaxException
+from ..datapack import DataPack
 
 NEW_LINE = '\n'
+
 AND_OPERATOR = '&&'
 OR_OPERATOR = '||'
 NOT_OPERATOR = '!'
+
+IF = True
+UNLESS = False
+
+VAR = '__logic__'
+
+count = 0
+
+
+@dataclass(eq=False, repr=True)
+class Condition:
+    string: str
+    if_unless: bool
+    """`True` means 'if', `False` means 'unless'"""
+
+    def reverse(self) -> None:
+        self.if_unless = not self.if_unless
+
+    def __str__(self) -> str:
+        return f"{'if' if self.if_unless else 'unless'} {self.string}"
+
+
+def merge_condition(conditions: list[Condition]) -> str:
+    return ' '.join(str(condition) for condition in conditions)
 
 
 def custom_condition(tokens: list[Token], tokenizer: Tokenizer) -> str:
@@ -46,7 +73,7 @@ def find_operator(_tokens: list[Token], operator: str, tokenizer: Tokenizer) -> 
     return list_of_tokens
 
 
-def __parse_condition(tokens: list[Token], tokenizer: Tokenizer) -> Union[dict, str]:
+def condition_to_ast(tokens: list[Token], tokenizer: Tokenizer) -> Union[dict, str]:
     if len(tokens) == 1 and tokens[0].token_type == TokenType.paren_round:
         if tokens[0].string == '()':
             raise JMCSyntaxException(
@@ -58,7 +85,7 @@ def __parse_condition(tokens: list[Token], tokenizer: Tokenizer) -> Union[dict, 
     list_of_tokens = find_operator(tokens, OR_OPERATOR, tokenizer)
     if len(list_of_tokens) > 1:
         return {"operator": OR_OPERATOR, "body": [
-            __parse_condition(tokens, tokenizer) for tokens in list_of_tokens]}
+            condition_to_ast(tokens, tokenizer) for tokens in list_of_tokens]}
     else:
         tokens = list_of_tokens[0]
 
@@ -66,17 +93,76 @@ def __parse_condition(tokens: list[Token], tokenizer: Tokenizer) -> Union[dict, 
     list_of_tokens = find_operator(tokens, AND_OPERATOR, tokenizer)
     if len(list_of_tokens) > 1:
         return {"operator": AND_OPERATOR, "body": [
-            __parse_condition(tokens, tokenizer) for tokens in list_of_tokens]}
+            condition_to_ast(tokens, tokenizer) for tokens in list_of_tokens]}
     else:
         tokens = list_of_tokens[0]
 
     # NotOperator should have a body as either dict or string and not list
     tokens = tokenizer.split_tokens(tokens, [NOT_OPERATOR])
     if tokens[0].token_type == TokenType.keyword and tokens[0].string == NOT_OPERATOR:
-        return {"operator": AND_OPERATOR, "body":
-                __parse_condition(tokens[1:], tokenizer)}
+        return {"operator": NOT_OPERATOR, "body":
+                condition_to_ast(tokens[1:], tokenizer)}
 
     return custom_condition(tokens, tokenizer)
+
+
+def ast_to_commands(ast: Union[dict, str]) -> tuple[list[Condition], Optional[tuple[list[Condition], int]]]:
+    global count
+    if isinstance(ast, str):
+        return [Condition(ast, IF)], None
+
+    if ast["operator"] == AND_OPERATOR:
+        conditions: tuple[Condition] = []
+        precommand_conditions: tuple[list[Condition], int] = []
+        for body in ast["body"]:
+            condition, _precommand_conditions = ast_to_commands(body)  # noqa
+            conditions.extend(condition)
+            if _precommand_conditions is not None:
+                precommand_conditions.extend(_precommand_conditions)
+        if not precommand_conditions:
+            precommand_conditions = None
+        return conditions, precommand_conditions
+
+    if ast["operator"] == OR_OPERATOR:
+        _count = count
+        count += 1
+        precommand_conditions: tuple[list[Condition], int] = []
+        for body in ast["body"]:
+            condition, _precommand_conditions = ast_to_commands(body)
+            if _precommand_conditions is not None:
+                precommand_conditions.extend(_precommand_conditions)
+            precommand_conditions.append((condition, _count))
+
+        return [Condition(f"score {VAR}{_count} {DataPack.VAR_NAME} matches 1", IF)], precommand_conditions
+
+    if ast["operator"] == NOT_OPERATOR:
+        conditions, _precommand_conditions = ast_to_commands(ast["body"])
+        for condition in conditions:
+            condition.reverse()
+        return conditions, _precommand_conditions
+
+    raise ValueError("Invalid AST")
+
+
+def commands_to_strings(ast: Union[dict, str]) -> tuple[str, str]:
+
+    conditions, precommand_conditions = ast_to_commands(ast)
+    if precommand_conditions is None:
+        precommand = ""
+    else:
+        precommands = []
+        current_count = -1
+        for conditions_and_count in precommand_conditions:
+            if conditions_and_count[1] > current_count:
+                current_count += 1
+                precommands.append(
+                    f"scoreboard players reset {VAR}{current_count} {DataPack.VAR_NAME}")
+            precommands.append(
+                f"execute {merge_condition(conditions_and_count[0])} run scoreboards players set {VAR}{current_count} {DataPack.VAR_NAME} 1")
+        precommand = '\n'.join(precommands)
+
+    condition_string = merge_condition(conditions)
+    return condition_string, precommand
 
 
 def parse_condition(condition_token: Token, tokenizer: Tokenizer) -> tuple[str, str]:
@@ -92,6 +178,11 @@ commands = [
 return datapack.add_raw_private_function("if_else", commands)
 ```
 """
-    condition_dict = __parse_condition([condition_token], tokenizer)
-    from json import dumps
-    print(dumps(condition_dict, indent=2))
+    global count
+    count = 0
+    ast = condition_to_ast([condition_token], tokenizer)
+    condition, precommand = commands_to_strings(ast)
+    precommand += '\n'
+    print(precommand)
+    print('\n'+condition)
+    return condition, precommand
