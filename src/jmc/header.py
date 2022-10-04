cecd0d5c02b8055c5b44d0c24865a030cc892f11
WingedSeal <firm09719@gmail.com>
2022-10-04 08:50:24 +0700

    Add docstrings for header.py

--- src/jmc/header.py
+++ src/jmc/header.py
@@ -12,24 +12,63 @@ class Header(SingleTon):
     A SingleTon class containing all information from header 
     """
     file_read: set[str] = set()
+    """Set of files that was already read (to prevent reading the same file multiple times"""
     macros: dict[str, str] = {}
+    """Dictionary of keyword to replace and what to replace it with"""
     replaces: dict[str, str] = {}
+    """Dictionary of string to replace and what to replace it with"""
 
     @classmethod
     def clear(cls) -> None:
+        """
+        Reset the single object
+        """
         self = cls()
         self.file_read = set()
         self.macros = {}
         self.replaces = {}
 
     def add_file_read(self, path: Path) -> None:
+        """
+        Add path to file_read
+
+        :param path: Path to file that's read
+        """
         self.file_read.add(path.as_posix())
 
     def is_header_exist(self, path: Path) -> bool:
+        """
+        Check if header is already in file_read
+
+        :param path: Path to check
+        :return: Whether the file was already read
+        """
         return path.as_posix() in self.file_read
 
 
 def parse_header(header_str: str, file_name: str, parent_target: Path) -> Header:
+    """
+    Parse header and store the information in the header object
+
+    :param header_str: String that was read from the file
+    :param file_name: Header file's name
+    :param parent_target: Path to parent of the main jmc file
+    :raises HeaderSyntaxException: A line in the file doesn't start with '#'
+    :raises HeaderDuplicatedMacro: Define same macro twice
+    :raises HeaderSyntaxException: Too many/little argument for define
+    :raises HeaderSyntaxException: File name isn't wrapped in quote or angle bracket (For `#include`)
+    :raises HeaderFileNotFoundError: Can't find header file
+    :raises HeaderSyntaxException: Include same file twice
+    :raises HeaderSyntaxException: Whitespace found in header file's name
+    :raises NotImplementedError: WORKING ON `#replace`
+    :raises NotImplementedError: WORKING ON `#credit`
+    :raises HeaderSyntaxException: Directive (`#something`) is unrecognized
+    :return: Header singleton object
+
+    ..TODO::
+        implement replace and credit
+
+    """
     header = Header()
     lines = header_str.split("\n")
     for line, line_str in enumerate(lines):
@@ -86,10 +125,10 @@ def parse_header(header_str: str, file_name: str, parent_target: Path) -> Header
                     "Whitespace is not supported in header file name", file_name, line, line_str)
 
         elif args[0] == "replace":
-            raise Exception("Replace feature hasn't been implemented yet.")
+            raise NotImplementedError("Replace feature hasn't been implemented yet.")
 
         elif args[0] == "credit":
-            raise Exception("Credit feature hasn't been implemented yet.")
+            raise NotImplementedError("Credit feature hasn't been implemented yet.")
 
         else:
             raise HeaderSyntaxException(
