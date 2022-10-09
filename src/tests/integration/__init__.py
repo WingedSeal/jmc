599ae0d87d71688d1506c8e9f719d9efedf0fe5c
WingedSeal <firm09719@gmail.com>
2022-10-10 00:56:05 +0700

    Add tests to integration/__init__.py

--- src/tests/integration/__init__.py
+++ src/tests/integration/__init__.py
@@ -1,3 +1,7 @@
-from . import test_flow_controls
+from . import test_flow_controls, test_function, test_jmc_function, test_new, test_variable
 ALL = (test_flow_controls,
+       test_function,
+       test_jmc_function,
+       test_new,
+       test_variable
        )
