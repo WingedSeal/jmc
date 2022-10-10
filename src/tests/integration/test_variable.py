4175e7f3f7bd595ec6bfe83b7def79da64eb6ff3
WingedSeal <firm09719@gmail.com>
2022-10-10 22:21:47 +0700

    Add test_variable.py

--- src/tests/integration/test_variable.py
+++ src/tests/integration/test_variable.py
@@ -1,12 +1,11 @@
-import unittest
 import sys
-
-
 sys.path.append('./src')  # noqa
 
+import unittest
 from tests.utils import string_to_tree_dict
 from jmc.test_compile import JMCPack
 
+from jmc.exception import JMCSyntaxException
 
 class TestVariable(unittest.TestCase):
     def test_declaration(self):
@@ -31,9 +30,123 @@ scoreboard players add $x __variable__ 0
 scoreboard players add $y __variable__ 0
             """)
         )
-    def test_assignment(self): ...
-    def test_operations(self): ...
-    def test_increment(self): ...
+
+    def test_no_op_error(self):
+        with self.assertRaises(JMCSyntaxException):
+            JMCPack().set_jmc_file("""
+$x;
+        """).build()
+
+    def test_assignment(self):
+        pack = JMCPack().set_jmc_file("""
+$x = obj:var1;
+$y = 1;
+$z = $x;
+$z -> obj:var2;
+$z->obj:var3;
+        """).build()
+
+        self.assertDictEqual(
+            pack.built,
+            string_to_tree_dict("""
+> VIRTUAL/data/minecraft/tags/functions/load.json
+{
+  "values": [
+    "TEST:__load__"
+  ]
+}
+> VIRTUAL/data/TEST/functions/__load__.mcfunction
+scoreboard objectives add __variable__ dummy
+scoreboard objectives add __int__ dummy
+scoreboard players operation $x __variable__ = var1 obj
+scoreboard players set $y __variable__ 1
+scoreboard players operation $z __variable__ = $x __variable__
+scoreboard players operation var2 obj = $z __variable__
+scoreboard players operation var3 obj = $z __variable__
+            """)
+        )
+
+    def test_operations(self):
+        pack = JMCPack().set_jmc_file("""
+$x += 1;
+$x *= 2;
+$x /= 3;
+$x += obj:var;
+$x -= $y;
+        """).build()
+
+        self.assertDictEqual(
+            pack.built,
+            string_to_tree_dict("""
+> VIRTUAL/data/minecraft/tags/functions/load.json
+{
+  "values": [
+    "TEST:__load__"
+  ]
+}
+> VIRTUAL/data/TEST/functions/__load__.mcfunction
+scoreboard objectives add __variable__ dummy
+scoreboard objectives add __int__ dummy
+scoreboard players set 2 __int__ 2
+scoreboard players set 3 __int__ 3
+scoreboard players add $x __variable__ 1
+scoreboard players operation $x __variable__ *= 2 __int__
+scoreboard players operation $x __variable__ /= 3 __int__
+scoreboard players operation $x __variable__ += var obj
+scoreboard players operation $x __variable__ -= $y __variable__
+            """)
+        )
+
+    def test_increment(self):
+        pack = JMCPack().set_jmc_file("""
+$x ++;
+$x++;
+$x--;
+        """).build()
+
+        self.assertDictEqual(
+            pack.built,
+            string_to_tree_dict("""
+> VIRTUAL/data/minecraft/tags/functions/load.json
+{
+  "values": [
+    "TEST:__load__"
+  ]
+}
+> VIRTUAL/data/TEST/functions/__load__.mcfunction
+scoreboard objectives add __variable__ dummy
+scoreboard objectives add __int__ dummy
+scoreboard players add $x __variable__ 1
+scoreboard players add $x __variable__ 1
+scoreboard players remove $x __variable__ 1
+            """)
+        )
+
+    def test_get(self):
+        pack = JMCPack().set_jmc_file("""
+execute store result score @s obj run $x.get();
+        """).build()
+
+        self.assertDictEqual(
+            pack.built,
+            string_to_tree_dict("""
+> VIRTUAL/data/minecraft/tags/functions/load.json
+{
+  "values": [
+    "TEST:__load__"
+  ]
+}
+> VIRTUAL/data/TEST/functions/__load__.mcfunction
+scoreboard objectives add __variable__ dummy
+scoreboard objectives add __int__ dummy
+execute store result score @s obj run scoreboard players get $x __variable__
+            """)
+        )
+
+        with self.assertRaises(JMCSyntaxException):
+            JMCPack().set_jmc_file("""
+$x.get(key=value);
+        """).build()
 
 
 if __name__ == '__main__':
