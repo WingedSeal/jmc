a612b9e1c413fbec824a31703da77850ec63e86d
WingedSeal <firm09719@gmail.com>
2022-10-11 14:36:37 +0700

    Add test_function.py

--- src/tests/integration/test_function.py
+++ src/tests/integration/test_function.py
@@ -5,12 +5,134 @@ import unittest
 from tests.utils import string_to_tree_dict
 from jmc.test_compile import JMCPack
 
+from jmc.exception import JMCSyntaxException
 
 class TestFunction(unittest.TestCase):
-    def test_define(self): ...
-    def test_call(self): ...
-    def test_anonymous(self): ...
-    def test_class(self): ...
+    def test_define(self):
+        pack = JMCPack().set_jmc_file("""
+function customFunction() {
+    say "Hello World";
+}
+        """).build()
+
+        self.assertDictEqual(
+            pack.built,
+            string_to_tree_dict("""
+> VIRTUAL/data/minecraft/tags/functions/load.json
+{
+  "values": [
+    "TEST:__load__"
+  ]
+}
+> VIRTUAL/data/TEST/functions/customfunction.mcfunction
+say Hello World
+> VIRTUAL/data/TEST/functions/__load__.mcfunction
+scoreboard objectives add __variable__ dummy
+scoreboard objectives add __int__ dummy
+            """)
+        )
+
+    def test_param_error(self):
+        with self.assertRaises(JMCSyntaxException):
+            JMCPack().set_jmc_file("""
+function customFunction(parameter) {
+    say "Hello World";
+}
+        """).build()
+
+        with self.assertRaises(JMCSyntaxException):
+            JMCPack().set_jmc_file("""
+customFunction(argument); 
+        """).build()
+
+    def test_call(self):
+        pack = JMCPack().set_jmc_file("""
+customFunction();
+        """).build()
+
+        self.assertDictEqual(
+            pack.built,
+            string_to_tree_dict("""
+> VIRTUAL/data/minecraft/tags/functions/load.json
+{
+  "values": [
+    "TEST:__load__"
+  ]
+}
+> VIRTUAL/data/TEST/functions/__load__.mcfunction
+scoreboard objectives add __variable__ dummy
+scoreboard objectives add __int__ dummy
+function TEST:customfunction
+            """)
+        )
+
+    def test_anonymous(self):
+        pack = JMCPack().set_jmc_file("""
+execute as @a run {
+    say "Hello World 1";
+    say "Hello World 2";
+}  
+        """).build()
+
+        self.assertDictEqual(
+            pack.built,
+            string_to_tree_dict("""
+> VIRTUAL/data/minecraft/tags/functions/load.json
+{
+  "values": [
+    "TEST:__load__"
+  ]
+}
+> VIRTUAL/data/TEST/functions/__load__.mcfunction
+scoreboard objectives add __variable__ dummy
+scoreboard objectives add __int__ dummy
+execute as @a run function TEST:__private__/anonymous/0
+> VIRTUAL/data/TEST/functions/__private__/anonymous/0.mcfunction
+say Hello World 1
+say Hello World 2
+            """)
+        )
+
+    def test_class(self):
+        pack = JMCPack().set_jmc_file("""
+class foo {
+    function bar() {
+        say "bar";
+    }
+    new advancements(bar) {
+        "criteria": {
+            "requirement": {
+            "trigger": "minecraft:tick"
+            }
+        }
+    }
+} 
+        """).build()
+
+        self.assertDictEqual(
+            pack.built,
+            string_to_tree_dict("""
+> VIRTUAL/data/minecraft/tags/functions/load.json
+{
+  "values": [
+    "TEST:__load__"
+  ]
+}
+> VIRTUAL/data/TEST/functions/foo/bar.mcfunction
+say bar
+> VIRTUAL/data/TEST/functions/__load__.mcfunction
+scoreboard objectives add __variable__ dummy
+scoreboard objectives add __int__ dummy
+> VIRTUAL/data/TEST/advancements/foo/bar.json
+{
+  "criteria": {
+    "requirement": {
+      "trigger": "minecraft:tick"
+    }
+  }
+}
+            """)
+        )
 
 
 class TestFeatures(unittest.TestCase):
