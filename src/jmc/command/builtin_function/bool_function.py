06a61ebf37197e4ae926c1ae1dc2a1531500881c
WingedSeal <firm09719@gmail.com>
2022-10-11 21:40:58 +0700

    Add func_property to TimerIsOver|

--- src/jmc/command/builtin_function/bool_function.py
+++ src/jmc/command/builtin_function/bool_function.py
@@ -1,23 +1,24 @@
 """Module containing JMCFunction subclasses for custom JMC function that returns a part of `/execute if` command"""
 
 from ..utils import ArgType
-from ..jmc_function import JMCFunction, FuncType
+from ..jmc_function import JMCFunction, FuncType, func_property
 
 IF = True
 UNLESS = False
 
 
-class TimerIsOver(JMCFunction):
-    func_type = FuncType.bool_function
-    call_string = 'Timer.isOver'
-    arg_type = {
+@func_property(
+    func_type=FuncType.load_once,
+    call_string='Timer.isOver',
+    arg_type={
         "objective": ArgType.keyword,
         "target_selector": ArgType.selector
-    }
-    name = 'timer_is_over'
-    defaults = {
+    },
+    name='timer_is_over',
+    defaults={
         "target_selector": "@s"
     }
-
+)
+class TimerIsOver(JMCFunction):
     def call_bool(self) -> tuple[str, bool]:
         return f'score {self.args["target_selector"]} {self.args["objective"]} matches 1..', UNLESS
