b087009967e27c860da581c99e4cf54d0d7171f1
WingedSeal <firm09719@gmail.com>
2022-10-13 20:23:29 +0700

    Make build.bat delete old file'

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 START /B /wait cmd /c "nuitka src/main.py --onefile --standalone --windows-icon-from-ico=./JMC-icon.ico --remove-output --output-dir=./dist"
 cd dist
+del "JMC.exe"
 ren "main.exe" "JMC.exe"
 cd ..
