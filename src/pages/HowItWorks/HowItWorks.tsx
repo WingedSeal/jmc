832f0755eae43a0107847b8139c041e2f9872b09
WingedSeal <firm09719@gmail.com>
2022-10-25 13:51:48 +0700

    Add HowItWorks

--- src/pages/HowItWorks/HowItWorks.tsx
+++ src/pages/HowItWorks/HowItWorks.tsx
@@ -1,29 +1,47 @@
 import React from "react";
 
 const HowItWorks = () => {
-    return <section className="min-h-screen bg-primary-dark flex flex-wrap pt-[14vh] md:pt-[15vh] px-4 md:px-11 flex-col items-centers">
-        <div className="text-primary text-3xl md:text-6xl mx-auto">How JMC works</div>
-        <div className="text-primary-contrast text-xl md:text-4xl mt-3 md:mt-4">
-            Processing JMC file
-        </div>
-        <div className="text-white text-base md:text-2xl">
-            <p>
-                &emsp;Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem cupiditate numquam sint id ipsa delectus, iusto vero ea rerum vel sequi ducimus corrupti modi, quos facere et. Odio, eaque corrupti?
-            </p>
-            <p className="mt-6">
-                &emsp;Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut beatae rem placeat, expedita maxime eligendi, est neque quisquam provident optio ratione totam sapiente. Corporis consequuntur ea accusantium voluptatem explicabo. Quisquam velit quo temporibus similique facilis, doloribus doloremque qui eius voluptatum.
-            </p>
-        </div>
-        <div className="text-primary-contrast text-xl md:text-4xl mt-3 md:mt-4">
-            Creating Minecraft Datapack
-        </div>
-        <div className="text-white text-base md:text-2xl">
-            <p>
-                &emsp;Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem cupiditate numquam sint id ipsa delectus, iusto vero ea rerum vel sequi ducimus corrupti modi, quos facere et. Odio, eaque corrupti?
-            </p>
-        </div>
-
-    </section>
+    return (
+        <section className="min-h-screen bg-primary-dark flex flex-wrap pt-[14vh] md:pt-[15vh] px-4 md:px-11 flex-col items-centers">
+            <div className="text-primary text-3xl md:text-6xl mx-auto">
+                How JMC works
+            </div>
+            <div className="text-primary-contrast text-xl md:text-4xl mt-3 md:mt-4">
+                Processing JMC file
+            </div>
+            <div className="text-white text-base md:text-2xl">
+                <p>
+                    &emsp;The moment you type <code>compile</code>, the
+                    compilation process starts. After reading the configuration,
+                    the JMC compiler takes your code from the main JMC file and
+                    parses it to tokens, and follows your imports accordingly.
+                </p>
+                <p className="mt-6">
+                    &emsp;Then it reads your header file (<code>main.hjmc</code>
+                    ) if there is one, and updates the tokens. Next, it takes
+                    care of all JMC-specific features using lexical analysis and
+                    creates a virtual datapack. Afterward, it writes that
+                    virtual datapack into the actual datapack in the output
+                    directory.
+                </p>
+            </div>
+            <div className="text-primary-contrast text-xl md:text-4xl mt-3 md:mt-4">
+                JMC certificate
+            </div>
+            <div className="text-white text-base md:text-2xl">
+                <p>
+                    &emsp;<code>jmc.txt</code> is a certificate file located at{" "}
+                    <code>./data/my_namespace/jmc.txt</code>. Its primary
+                    purpose is to prevent you from accidentally overriding your
+                    handmade datapack. It's for showing the compiler that this
+                    namespace was written by JMC and that the namespace is safe
+                    to be deleted. Another use of this file is to save the
+                    advanced configuration of JMC. You are allowed to change the
+                    content if you understand what you are doing.
+                </p>
+            </div>
+        </section>
+    );
 };
 
 export default HowItWorks;
--- src/pages/Introduction/Introduction.tsx
+++ src/pages/Introduction/Introduction.tsx
@@ -1,21 +1,31 @@
 import React from "react";
 
 const Introduction = () => {
-    return <section className="min-h-screen bg-secondary-dark flex flex-wrap pt-[14vh] md:pt-[15vh] px-4 md:px-11 flex-col items-centers">
-        <div className="text-secondary text-3xl md:text-6xl mx-auto">JMC Basics</div>
-        <div className="text-seconday-contrast text-xl md:text-4xl mt-3 md:mt-4">
-            Preprocessing
-        </div>
-        <div className="text-white text-base md:text-2xl">
-            <p>
-                &emsp;Sometimes, coding datapack in mcfunction has very annoying problems. To create a function, you need to create an entirely new file. There's no way to format your code, no in-line comment. There are algorithms you need to implement again and again. This is where a preprocessor can help.
-            </p>
-            <p className="mt-6">
-                &emsp;Once you start using JMC, it will take your preprocessed JMC file and save it as normal mcfunction files and the rest of the datapack.
-            </p>
-        </div>
-
-    </section>
+    return (
+        <section className="min-h-screen bg-secondary-dark flex flex-wrap pt-[14vh] md:pt-[15vh] px-4 md:px-11 flex-col items-centers">
+            <div className="text-secondary text-3xl md:text-6xl mx-auto">
+                JMC Basics
+            </div>
+            <div className="text-seconday-contrast text-xl md:text-4xl mt-3 md:mt-4">
+                Preprocessing
+            </div>
+            <div className="text-white text-base md:text-2xl">
+                <p>
+                    &emsp;Sometimes, coding datapack in mcfunction has very
+                    annoying problems. To create a function, you need to create
+                    an entirely new file. There's no way to format your code,
+                    and no in-line comment. There are algorithms you need to
+                    implement again and again. This is where a preprocessor can
+                    help.
+                </p>
+                <p className="mt-6">
+                    &emsp;Once you start using JMC, it will take your
+                    preprocessed JMC file and save it as normal mcfunction files
+                    and the rest of the datapack.
+                </p>
+            </div>
+        </section>
+    );
 };
 
 export default Introduction;
