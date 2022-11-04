1bd9fcaa163d2d46a6fc480cfbae0fdd8a3428a2
WingedSeal <firm09719@gmail.com>
2022-11-04 23:02:53 +0700

    Updage build

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-START /B /wait cmd /c "nuitka src/main.py --onefile --standalone --windows-icon-from-ico=./JMC-icon.ico --remove-output --output-dir=./dist --mingw64"
+START /B /wait cmd /c "nuitka src/jmc/__main__.py --onefile --standalone --windows-icon-from-ico=./JMC-icon.ico --remove-output --output-dir=./dist --mingw64"
 cd dist
 del "JMC.exe"
 ren "main.exe" "JMC.exe"
--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-nuitka3 src/main.py --onefile --standalone --linux-icon=./JMC-icon.ico --remove-output --output-dir=./dist
+nuitka3 src/jmc/__main__.py --onefile --standalone --linux-icon=./JMC-icon.ico --remove-output --output-dir=./dist
 cd dist
 if [ -f JMC.bin ]; then
    rm JMC.bin
