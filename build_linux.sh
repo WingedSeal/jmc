507a4b2263673a29e99f8e8e900a52bbe03b8b2b
WingedSeal <firm09719@gmail.com>
2022-11-04 23:16:06 +0700

    Update build_linux

--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-nuitka3 src/jmc/run.py --onefile --standalone --linux-icon=./JMC-icon.ico --remove-output --output-dir=./dist
+nuitka3 src/run.py --onefile --standalone --linux-icon=./JMC-icon.ico --remove-output --output-dir=./dist
 cd dist
 if [ -f JMC.bin ]; then
    rm JMC.bin
