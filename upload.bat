b650cf90ea7a7cfaae01d6ff460224b1702c94ef
WingedSeal <firm09719@gmail.com>
2022-12-15 18:14:31 +0700

    Update upload.bat

--- upload.bat
+++ upload.bat
@@ -2,7 +2,7 @@ cd src/dist
 del jmcfunction* /a
 cd ..
 python setup.py sdist bdist_wheel 
-twine upload dist/jmcfunction*
+START /B /wait cmd /c "twine upload dist/jmcfunction*"
 rmdir /s jmcfunction.egg-info
 rmdir /s build
 cd ..
