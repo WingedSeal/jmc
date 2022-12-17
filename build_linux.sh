7e0619971129b1807db10595a3a19b6b789507c8
Poly <84731064+Poly2it@users.noreply.github.com>
2022-12-17 18:15:03 +0100

    Append version name to build output file name
    This change appends the version number in `__main__.py` to the output file name when building for Linux.

--- build_linux.sh
+++ build_linux.sh
@@ -1,9 +1,16 @@
 #!/bin/bash 
 
+REGEX_VERSION="(?<=').*?(?=')"
+# Fetch the 7th line of `__main__.py`, which contains the VERSION variable.
+# TODO: don't rely on the fact that VERSION is the 7th line of code.
+VERSION=$(sed '7!d' src/jmc/__main__.py)
+# Apply regex to separate the contained value.
+VERSION=$(echo "$VERSION" | grep -P "$REGEX_VERSION" -o)
+
 nuitka3 src/run.py --onefile --standalone --linux-icon=./JMC-icon.ico --remove-output --output-dir=./dist
 cd dist
 if [ -f JMC.bin ]; then
    rm JMC.bin
 fi
-mv run.bin JMC.bin
+mv run.bin JMC-$VERSION.bin
 cd ..
