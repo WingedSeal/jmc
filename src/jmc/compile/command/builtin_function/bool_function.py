fa79351267b8a01f2106f0c61607493ab8655086
WingedSeal <firm09719@gmail.com>
2022-12-26 19:43:43 +0700

    Add Object.isEqual

--- src/jmc/compile/command/builtin_function/bool_function.py
+++ src/jmc/compile/command/builtin_function/bool_function.py
@@ -31,7 +31,7 @@ class TimerIsOver(JMCFunction):
     call_string="String.isEqual",
     arg_type={
         "type": ArgType.KEYWORD,
-        "source": ArgType.KEYWORD,
+        "source": ArgType.STRING,
         "path": ArgType.KEYWORD,
         "string": ArgType.STRING
     },
@@ -54,4 +54,41 @@ class StringIsEqual(JMCFunction):
         return f"score {bool_result} {self.datapack.var_name} matches 0", IF, [
             f"data modify storage {self.datapack.namespace}:{self.datapack.storage_name} currentObject set from {self.args['type']} {source} {self.args['path']}",
             f"execute store success score {bool_result} {self.datapack.var_name} run data modify storage {self.datapack.namespace}:{self.datapack.storage_name} {self.current_object} set value {self.args['string']}"
+        ]
+
+
+@func_property(
+    func_type=FuncType.BOOL_FUNCTION,
+    call_string="Object.isEqual",
+    arg_type={
+        "type1": ArgType.KEYWORD,
+        "source1": ArgType.STRING,
+        "path1": ArgType.KEYWORD,
+        "type2": ArgType.KEYWORD,
+        "source2": ArgType.STRING,
+        "path2": ArgType.KEYWORD,
+    },
+    name="object_is_equal"
+)
+class ObjectIsEqual(JMCFunction):
+    current_object = "currentObject"
+
+    def call_bool(self) -> tuple[str, bool, list[str]]:
+        bool_result = self.datapack.data.get_current_bool_result()
+        source1 = self.args["source1"]
+        source2 = self.args["source2"]
+        for param in ("type1", "type2"):
+            if self.args[param] not in {"storage", "block", "entity"}:
+                raise JMCValueError(
+                    f"'type' parameter expect 'storage' or 'block' or 'entity' (got {self.args[param]})",
+                    self.raw_args[param].token,
+                    self.tokenizer)
+        if self.args["type1"] == "storage" and ":" not in self.args["source1"]:
+            source1 = f"{self.datapack.namespace}:{source1}"
+        if self.args["type2"] == "storage" and ":" not in self.args["source2"]:
+            source1 = f"{self.datapack.namespace}:{source2}"
+
+        return f"score {bool_result} {self.datapack.var_name} matches 0", IF, [
+            f"data modify storage {self.datapack.namespace}:{self.datapack.storage_name} currentObject set from {self.args['type1']} {source1} {self.args['path1']}",
+            f"execute store success score {bool_result} {self.datapack.var_name} run data modify storage {self.datapack.namespace}:{self.datapack.storage_name} {self.current_object} set from {self.args['type2']} {source2} {self.args['path2']}"
         ]
