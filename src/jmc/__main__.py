163b3ada89ab297c7ca0ab208400ce95ecf0c407
WingedSeal <firm09719@gmail.com>
2022-12-26 20:00:13 +0700

    Update version

--- src/jmc/__main__.py
+++ src/jmc/__main__.py
@@ -4,7 +4,7 @@ from .compile import Logger
 from .terminal.utils import RestartException, handle_exception
 from .terminal import GlobalData, Colors, start
 
-VERSION = "v1.2.9"
+VERSION = "v1.2.10-alpha.1"
 CONFIG_FILE_NAME = "jmc_config.json"
 GlobalData().init(VERSION, CONFIG_FILE_NAME)
 
