4b7de47d586c46e158f52f5a1d47a75103ed61b6
AmericanBagel <92078772+AmericanBagel@users.noreply.github.com>
2023-01-08 20:26:53 -0800

    Add code_test_linux.sh

---
+++ code_test_linux.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "====================================="
+echo "^> Code Cyclomatic Complexity (radon)"
+echo "====================================="
+radon cc src/jmc --min C -s --total-average
+echo "====================================="
+echo "^> Code Maintainability Index (radon) "
+echo "====================================="
+radon mi src/jmc --min B -s
+echo "=================================="
+echo "^> Unit/Integration Tests (python)"
+echo "=================================="
+python src/tests/test_all.py || exit 1
+echo "===================="
+echo "^> Type hints (mypy)"
+echo "===================="
+mypy ./src/jmc
+echo "================================="
+echo "^> Code Style(PEP8) (pycodestyle)"
+echo "================================="
+pycodestyle ./src --ignore=E501,W50
+# pylint src/jmc --disable=C0301,W1201,W1203
