84bcc4d784c78aa7666e5780de586ec548688f45
Nico314159 <nicolino.will@gmail.com>
2023-11-19 20:43:49 -0800

    Fix swapped main and subcommands in dict
    weather and item are subcommands of playsound and particle, not the reverse.

--- src/jmc/compile/lexer_func_content.py
+++ src/jmc/compile/lexer_func_content.py
@@ -59,8 +59,8 @@ FIRST_ARGUMENTS_EXCEPTION = {
     "item": {"summon"},
     "loot": {"loot", "give"},
     "data": {"execute"},
-    "playsound": {"weather"},
-    "particle": {"item"}
+    "weather": {"playsound"},
+    "item": {"particle"}
 }
 """Dictionary of (FIRST_ARGUMENTS that can also be used as normal argument in a command) and (those commands)
 
