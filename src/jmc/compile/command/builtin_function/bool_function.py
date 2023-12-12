7e7e264451efe38276997a1152789933c45ca22a
amandin <123622161+amawdiin@users.noreply.github.com>
2023-12-12 20:09:09 +0600

    fix regex not working for ~ ~1.1 ~ and ^ ^1.1 ^

--- src/jmc/compile/command/builtin_function/bool_function.py
+++ src/jmc/compile/command/builtin_function/bool_function.py
@@ -34,7 +34,7 @@ class nbtSource:
     def get_source_type(source) -> str:
         if source.startswith("@") or nbtSource.is_uuid(source):
             source_type = "entity"
-        elif re.match(r'^[~\^]?-?\d+(\.\d+)?\s[~\^]?-?\d+(\.\d+)?\s[~\^]?-?\d+(\.\d+)?$', source):
+        elif re.match(r'^[~\^]?-?\d*(\.\d+)?\s[~\^]?-?\d*(\.\d+)?\s[~\^]?-?\d*(\.\d+)?[~\^]?$', source):
             source_type = "block"
         else:
             source_type = "storage"
