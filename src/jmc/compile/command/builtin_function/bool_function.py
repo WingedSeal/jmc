b7d4f9df46a88cb2963be5bf7c5eb3d9b6328288
amandin <123622161+amawdiin@users.noreply.github.com>
2023-12-14 14:58:02 +0600

    Remove `type` parameter from `Object.isEqual()` and `String.isEqual()` (#71)
    * Object.isEqual() doesnt require type now
    
    * Added check for UUID to avoid edgecases
    
    * isEqual doesnt require type arguments now.
    
    * class typo fixed
    
    * removed print for debugging
    
    * fix block being treated as storage
    
    * debug message brought back
    
    * use regex to detect coordinates
    
    * fix forward slashes in regex
    
    * remove debug message(again)
    
    * removed type from arguments for String.isEqual
    
    * fix both types being produced from the same source
    
    * minecraft coordinates regex modified to match decimal coordinates too.
    
    * fix regex not working for ~ ~1.1 ~ and ^ ^1.1 ^
    
    * is_uuid() -> bool instead of str
    
    * add types to arguments in funcs under nbtSource class
    
    * comment added
    
    * fixed overriding 'type' variable.
    
    * probably fixed something that wingedseal told me to do
    
    * testing, trying to get instantiation thingy to work
    
    * further testing
    
    * even more testing
    
    * oooh i think i got it now!!
    
    * hmm maybe if i do this
    
    * if that works imma go cry
    
    * no message
    
    * THIS HAS TO BE IT
    
    * YAY THATS IT
    
    * fixed class issues and return thingy that seal told me to fix
    
    * string documentation
    
    * used jmc's docstring format, removed unnecessary class.
    
    * yea i think i deleted some of the newlines for no reason, brought em back tho

--- src/jmc/compile/command/builtin_function/bool_function.py
+++ src/jmc/compile/command/builtin_function/bool_function.py
@@ -3,9 +3,34 @@
 from ..utils import ArgType
 from ..jmc_function import JMCFunction, FuncType, func_property
 from ...exception import JMCValueError
+import re
 
 IF = True
 UNLESS = False
+
+
+def is_uuid(source: str) -> bool:
+    """
+    Tells whether source of the data is minecraft UUID or not
+
+    :param source: Source of the data
+    :return: True | False
+    """
+    parts = source.split('-')
+    return len(parts) == 5 and all(len(part) in (8, 4, 4, 4, 12) and part.isalnum() for part in parts)
+    
+def get_nbt_type(source: str) -> str:
+    """
+    Gets the type of data based on the data source.
+
+    :param source: Source of the data
+    :return: A string representing the type of NBT data
+    """
+    if source.startswith("@") or is_uuid(source):
+        return "entity"
+    elif re.match(r'^[~\^]?-?\d*(\.\d+)?\s[~\^]?-?\d*(\.\d+)?\s[~\^]?-?\d*(\.\d+)?[~\^]?$', source): # checks if the string is block coord with regex
+        return "block"
+    return "storage"
 
 
 @func_property(
@@ -30,7 +55,6 @@ class TimerIsOver(JMCFunction):
     func_type=FuncType.BOOL_FUNCTION,
     call_string="String.isEqual",
     arg_type={
-        "type": ArgType.KEYWORD,
         "source": ArgType.STRING,
         "path": ArgType.KEYWORD,
         "string": ArgType.STRING
@@ -43,16 +67,12 @@ class StringIsEqual(JMCFunction):
     def call_bool(self) -> tuple[str, bool, list[str]]:
         bool_result = self.datapack.data.get_current_bool_result()
         source = self.args["source"]
-        if self.args["type"] not in {"storage", "block", "entity"}:
-            raise JMCValueError(
-                f"'type' parameter expect 'storage' or 'block' or 'entity' (got {self.args['type']})",
-                self.raw_args["type"].token,
-                self.tokenizer)
-        if self.args["type"] == "storage" and ":" not in self.args["source"]:
+        source_type = get_nbt_type()
+        if source_type == "storage" and ":" not in self.args["source"]:
             source = f"{self.datapack.namespace}:{source}"
 
         return f"score {bool_result} {self.datapack.var_name} matches 0", IF, [
-            f"data modify storage {self.datapack.namespace}:{self.datapack.storage_name} currentObject set from {self.args['type']} {source} {self.args['path']}",
+            f"data modify storage {self.datapack.namespace}:{self.datapack.storage_name} currentObject set from {source_type} {source} {self.args['path']}",
             f"execute store success score {bool_result} {self.datapack.var_name} run data modify storage {self.datapack.namespace}:{self.datapack.storage_name} {self.current_object} set value {self.args['string']}"
         ]
 
@@ -61,10 +81,8 @@ class StringIsEqual(JMCFunction):
     func_type=FuncType.BOOL_FUNCTION,
     call_string="Object.isEqual",
     arg_type={
-        "type1": ArgType.KEYWORD,
         "source1": ArgType.STRING,
         "path1": ArgType.KEYWORD,
-        "type2": ArgType.KEYWORD,
         "source2": ArgType.STRING,
         "path2": ArgType.KEYWORD,
     },
@@ -77,18 +95,14 @@ class ObjectIsEqual(JMCFunction):
         bool_result = self.datapack.data.get_current_bool_result()
         source1 = self.args["source1"]
         source2 = self.args["source2"]
-        for param in ("type1", "type2"):
-            if self.args[param] not in {"storage", "block", "entity"}:
-                raise JMCValueError(
-                    f"'type' parameter expect 'storage' or 'block' or 'entity' (got {self.args[param]})",
-                    self.raw_args[param].token,
-                    self.tokenizer)
-        if self.args["type1"] == "storage" and ":" not in self.args["source1"]:
+        type1 = get_nbt_type(source1)
+        type2 = get_nbt_type(source2)
+        if type1 == "storage" and ":" not in self.args["source1"]:
             source1 = f"{self.datapack.namespace}:{source1}"
-        if self.args["type2"] == "storage" and ":" not in self.args["source2"]:
+        if type2 == "storage" and ":" not in self.args["source2"]:
             source2 = f"{self.datapack.namespace}:{source2}"
 
         return f"score {bool_result} {self.datapack.var_name} matches 0", IF, [
-            f"data modify storage {self.datapack.namespace}:{self.datapack.storage_name} currentObject set from {self.args['type1']} {source1} {self.args['path1']}",
-            f"execute store success score {bool_result} {self.datapack.var_name} run data modify storage {self.datapack.namespace}:{self.datapack.storage_name} {self.current_object} set from {self.args['type2']} {source2} {self.args['path2']}"
+            f"data modify storage {self.datapack.namespace}:{self.datapack.storage_name} currentObject set from {type1} {source1} {self.args['path1']}",  # noqa
+            f"execute store success score {bool_result} {self.datapack.var_name} run data modify storage {self.datapack.namespace}:{self.datapack.storage_name} {self.current_object} set from {type2} {source2} {self.args['path2']}"  # noqa
         ]
