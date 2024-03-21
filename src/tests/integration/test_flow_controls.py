f058f5d9533a3cc61dae493a9ecaaeb0472dd6de
WingedSeal <firm09719@gmail.com>
2024-03-21 14:28:23 +0700

    Fix test case

--- src/tests/integration/test_flow_controls.py
+++ src/tests/integration/test_flow_controls.py
@@ -53,12 +53,10 @@ if (entity condition) {
 scoreboard objectives add __variable__ dummy
 scoreboard players set __if_else__ __variable__ 0
 execute if entity condition run function TEST:__private__/if_else/0
-execute if score __if_else__ __variable__ matches 0 run function TEST:__private__/if_else/1
+execute if score __if_else__ __variable__ matches 0 run say FALSE
 > VIRTUAL/data/TEST/functions/__private__/if_else/0.mcfunction
 say TRUE
 scoreboard players set __if_else__ __variable__ 1
-> VIRTUAL/data/TEST/functions/__private__/if_else/1.mcfunction
-say FALSE
             """)
         )
 
@@ -84,14 +82,9 @@ if (entity condition) {
 scoreboard objectives add __variable__ dummy
 scoreboard players set __if_else__ __variable__ 0
 execute if entity condition run function TEST:__private__/if_else/0
-execute if score __if_else__ __variable__ matches 0 run function TEST:__private__/if_else/1
+execute if score __if_else__ __variable__ matches 0 if entity condition2 run say CONDITION2
 > VIRTUAL/data/TEST/functions/__private__/if_else/0.mcfunction
 say CONDITION1
-scoreboard players set __if_else__ __variable__ 1
-> VIRTUAL/data/TEST/functions/__private__/if_else/1.mcfunction
-execute if entity condition2 run function TEST:__private__/if_else/2
-> VIRTUAL/data/TEST/functions/__private__/if_else/2.mcfunction
-say CONDITION2
 scoreboard players set __if_else__ __variable__ 1
             """)
         )
