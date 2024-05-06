94b8793b8bb2194613f069c2cb01227301bbca9e
WingedSeal <firm09719@gmail.com>
2024-05-06 18:10:05 +0700

    feat: is_connected

--- src/jmc/compile/utils.rs
+++ src/jmc/compile/utils.rs
@@ -1,3 +1,17 @@
+use super::tokenizer::Token;
+
+#[inline]
 pub fn is_decorator(string: &str) -> bool {
     return string.len() > 2 && string.starts_with("@");
+}
+
+#[inline]
+/// Whether 2 tokens are next to each other
+pub fn is_connected(current_token: &Token, previous_token: &Token) -> bool {
+    if previous_token._macro_length == 0 {
+        previous_token.line == current_token.line
+            && previous_token.col + previous_token.length() as u32 == current_token.col
+    } else {
+        todo!()
+    }
 }
