462f016ad872fdc2f4aed9af0e11ab48333dfca0
WingedSeal <firm09719@gmail.com>
2025-06-04 21:00:34 +0700

    feat: add \ to operator

--- src/jmc/compile/tokenizer.py
+++ src/jmc/compile/tokenizer.py
@@ -170,7 +170,8 @@ PAREN_PAIR = {
 }
 """Dictionary of left bracket(string) and right bracket(string)"""
 
-OPERATORS = {"+", "-", "*", "/", ">", "<", "=", "%", ":", "!", "|", "&", "?"}
+OPERATORS = {"+", "-", "*", "/", ">", "<",
+             "=", "%", ":", "!", "|", "&", "?", "\\"}
 
 
 class Tokenizer:
